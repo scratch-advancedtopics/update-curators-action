ddd99974ea1c462e2b206c12f9dc88717005cb04
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-10-31 20:10:29 +0000

    aaa

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,7 +1,7 @@
 echo "commit.sh started"
 cd $GITHUB_WORKSPACE
-git config --user.name "ScratchATs-bot[bot]"
-git config --user.email "ScratchATs-bot[bot]@users.noreply.github.com"
+git config --local user.name "ScratchATs-bot[bot]"
+git config --local user.email "ScratchATs-bot[bot]@users.noreply.github.com"
 echo "[commit.sh]: committing locally"
 git commit -m -a "update members"
 echo "[commit.sh]: pushing to github"
