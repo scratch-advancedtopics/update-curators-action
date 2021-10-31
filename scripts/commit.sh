5fac70c89b882c2f1367504d9b849c2bcae8731f
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-10-31 20:14:20 +0000

    global? maybe that should fix things

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,7 +1,7 @@
 echo "commit.sh started"
 cd $GITHUB_WORKSPACE
-git config --local user.name "ScratchATs-bot[bot]"
-git config --local user.email "ScratchATs-bot[bot]@users.noreply.github.com"
+git config --global user.name "ScratchATs-bot[bot]"
+git config --global user.email "ScratchATs-bot[bot]@users.noreply.github.com"
 echo "[commit.sh]: committing locally"
 git commit -m "update members" -a
 echo "[commit.sh]: pushing to github"
