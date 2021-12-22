696de3bd0d2581a5555091b1d6b63b1ffb1592f5
Metaify <67440879+FunctionalMetatable@users.noreply.github.com>
2021-12-22 18:38:51 +0000

    Update commit.sh

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,9 +1,9 @@
 echo "commit.sh started"
 cd $GITHUB_WORKSPACE
-git config --global user.name "ScratchATs-bot[bot]"
-git config --global user.email "ScratchATs-bot[bot]@users.noreply.github.com"
+git config --global user.name "FunctionalMetatable"
+git config --global user.email "FunctionalMetatable@users.noreply.github.com"
 echo "[commit.sh]: committing locally"
-git commit -m "update members" -a
+git commit -m "[update-curators-action] Updating members" -a
 echo "[commit.sh]: pushing to github"
 git push
 echo "completed commit.sh"
