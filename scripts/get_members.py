18b6630cb12e341de2b749ce0af69ca6d8ebf4e2
Metaify <67440879+FunctionalMetatable@users.noreply.github.com>
2022-01-13 19:37:45 +0000

    Fix python script

--- scripts/get_members.py
+++ scripts/get_members.py
@@ -1,29 +1,46 @@
-import urllib.request
-import json
-import sys
+from os import environ
+from urllib import request
+import json, sys
 
-def get_users(status):
-	offset = 0
-	out = []
-	while True:
-		contents = urllib.request.urlopen(f"https://api.scratch.mit.edu/studios/30152868/{status}?offset={offset}").read()
-		contents = contents.decode('utf-8')
-		if contents == '[]':
-			break
-		out.append(contents)
-		offset += 20
-	return out
+def get_members_with_status(status):
+    out = []
+    offset = 0
+    limit = 24
+
+    while True:
+        req = request.urlopen(f"https://api.scratch.mit.edu/studios/30152868/{status}s?offset={offset}&limit={limit}").read()
+        members = json.loads(req.decode("utf-8"))
+
+        for member in members:
+            print(f"Found {status} \033[96m{member['username']}\033[0m#\033[92m{member['id']}\033[0m - Request #: {int(((offset + limit) / limit))}")
+            out.append(member)
+        
+        if len(members) == 0 or len(members) < 24:
+            break
+
+        offset += limit
+    
+    return out
 
 def main():
-	out = []
-	for i in get_users('curators'):
-		i = json.loads(i)
-		out.append(i)
-	for i in get_users('managers'):
-		i = json.loads(i)
-		out.append(i)
-	return out
+    members = [
+        get_members_with_status("manager"),
+        get_members_with_status("curator")
+    ]
 
-print(f"workspace: {sys.argv[1]}")
-with open(f'{sys.argv[1]}/members.json','w') as f:
-	json.dump(main(), f)
+    return members
+
+workspace = environ.get("GITHUB_WORKSPACE")
+
+if workspace == None:
+    if len(sys.argv) < 1:
+        workspace = sys.argv[1] + "/"
+    else:
+        workspace = ""
+else:
+    workspace += "/"
+
+
+
+with open(f'{workspace}members.json', "w") as file:
+    json.dump(main(), file)
